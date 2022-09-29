ac1a6cd73e77efd5a005684fbe09a5b01341f43a
wagiboy <wagiboy@gmail.com>
2022-09-29 14:25:25 +0200

    added a footer

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@
     <v-main>
       <router-view/>
     </v-main>
+    <v-footer padless class="white">
+      <v-col cols="12" class="text-center blue-grey--text caption">
+        <v-icon small>mdi-xml</v-icon>
+        {{ new Date().getFullYear() }} — Dirk Wagner
+      </v-col>
+    </v-footer>
   </v-app>
 </template>
 
